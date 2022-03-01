d9c28ca860fa5b3baa134e12751a577955f131e8
laptopadventure <bazelart13@gmail.com>
2022-02-28 20:08:00 -0800

    challenge 4

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -45,7 +45,11 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 ------------------------------------------------------------------------------------------------ */
 
 const forLoopTwoToThe = (arr) => {
-  // Solution code here...
+  let output = [];
+  for(let power of arr) {
+    output.push(Math.pow(2, power));
+  }
+  return output;
 };
 
 /* ------------------------------------------------------------------------------------------------
