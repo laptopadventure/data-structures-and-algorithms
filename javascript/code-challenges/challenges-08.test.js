38323d6a2deb054d2647299500bbe2682b663964
laptopadventure <bazelart13@gmail.com>
2022-03-22 17:40:55 -0700

    challenge 2

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -67,12 +67,13 @@ const sortByChildren = (charArray) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
 
-Write a function named containsW that takes in a string. This function should use a regular expression pattern to return true if the string contains the letter 'w' in lower case or false if it does not. 
+Write a function named containsW that takes in a string. This function should use a regular expression pattern to return true if the string contains the letter 'w' in lower case or false if it does not.
 
 ------------------------------------------------------------------------------------------------ */
 
 const containsW = (str) => {
-  // Solution code here...
+  const regex = /w/;
+  return !!str.match(regex);
 };
 
 /* ------------------------------------------------------------------------------------------------
