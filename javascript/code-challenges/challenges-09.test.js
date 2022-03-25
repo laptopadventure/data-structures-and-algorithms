a1475b69ac5c5016ba4a9034a328b5602476a3c6
laptopadventure <bazelart13@gmail.com>
2022-03-24 20:30:28 -0700

    challenge 3

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -46,7 +46,13 @@ Write a function named checkValues that takes in an object and a value and retur
 ------------------------------------------------------------------------------------------------ */
 
 const checkValues = (obj, value) => {
-  // Solution code here...
+  for(let key in obj) {
+    let val = obj[key];
+    if(val === value) {
+      return true;
+    }
+  }
+  return false;
 };
 
 /* ------------------------------------------------------------------------------------------------
