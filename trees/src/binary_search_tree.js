dfbc395adfe927021dd76336f9a705ffe8a9dd44
laptopadventure <bazelart13@gmail.com>
2022-09-08 18:49:29 -0700

    trees max stuff

--- trees/src/binary_search_tree.js
+++ trees/src/binary_search_tree.js
@@ -20,7 +20,6 @@ class BinarySearchTree extends BinaryTree {
       //right
       if (number > currentNode.value) {
         if (currentNode.right) {
-          console.log("going right");
           currentNode = currentNode.right;
         } else {
           currentNode.right = new Node(number);
@@ -28,10 +27,51 @@ class BinarySearchTree extends BinaryTree {
         }
       }
     }
-    console.log("lost");
   }
   contains(number) {
     return this.inOrder().includes(number);
+  }
+
+  //boring solution
+  findMaximumValue() {
+    const arr = this.inOrder();
+    return arr[arr.length - 1];
+  }
+  //cool solution
+  findMaximumValueCool() {
+    let current = this.root;
+    if (!current) {
+      return undefined;
+    }
+    let maximumValue = current.value;
+    while (true) {
+      if (maximumValue < current.value) {
+        maximumValue = current.value;
+      }
+      if (!current.left) {
+        if (!current.right) {
+          console.log("no left, no right, done");
+          break;
+        } else {
+          current = current.right;
+          console.log("no left, moving right to", current.value);
+          continue;
+        }
+      }
+      if (!current.right) {
+        current = current.left;
+        console.log("no right, moving left to", current.value);
+        continue;
+      }
+      if (current.left.value > current.right.value) {
+        current = current.left;
+        console.log("left greater than right, moving left to", current.value);
+      } else {
+        current = current.right;
+        console.log("right greater than left, moving right to", current.value);
+      }
+    }
+    return maximumValue;
   }
 }
 
